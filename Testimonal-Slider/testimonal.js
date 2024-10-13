d7f3a14fd6028c9f415df8dc6c5f6d6e8d77c46b
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-10-13 20:30:20 +0530

    implement the  data

--- Testimonal-Slider/testimonal.js
+++ Testimonal-Slider/testimonal.js
@@ -1,8 +1,49 @@
-const data = [
+
+
+
+const testimonals= [
   {
     name: 'Ayan',
     text: 'Silence.....the only language i fluent in.',
     imgUrl:'images\photo-1570295999919-56ceb5ecca61.avif'
+  },
+  {
+    name: 'Anam',
+    text: 'Silence.....the only language i fluent in.',
+    imgUrl:'images\photo-1570295999919-56ceb5ecca61.avif'
+  },
+  {
+    name: 'Ashar',
+    text: 'Silence.....the only language i fluent in.',
+    imgUrl:'images\photo-1570295999919-56ceb5ecca61.avif'
   }
 ];
-console.log(data);
+
+testimonals.forEach((testimonal) => {
+ const name = testimonal.name;
+ const text = testimonal.text;
+ const imgUrl = testimonal.imgUrl;
+
+ function updateTestimonal(){
+  document.querySelector('.js-container').innerHTML = `
+  <img class="image" src="${imgUrl}">
+     <p class="text">
+      ${text}
+     </p>
+     <p class="writer-name">${name}</p>
+  `;
+
+  setTimeout(() => {
+    updateTestimonal()
+  },10000);
+ }
+
+updateTestimonal();
+
+ 
+
+ 
+});
+
+}
+
